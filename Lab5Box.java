05eeb50277bfc2bd7e10d86fb65c21d94087826a
Saiansh Nair <65238028+Saiansh19@users.noreply.github.com>
2025-06-07 20:00:26 +0530

    Update and rename Box.java to Lab5Box.java

--- Lab5Box.java
+++ Lab5Box.java
@@ -1,21 +1,21 @@
-class Box {
+class Lab5Box {
     double length;
     double width;
     double height;
 
-    public Box(double length, double width, double height) {
+    public Lab5Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
-    public Box(double length, double width) {
+    public Lab5Box(double length, double width) {
         this.length = length;
         this.width = width;
         this.height = 1;
     }
 
-    public Box(double length) {
+    public Lab5Box(double length) {
         this.length = length;
         this.width = 1;
         this.height = 1;
@@ -26,13 +26,13 @@ class Box {
     }
 
     public static void main(String[] args) {
-        Box box1 = new Box(5, 3, 2);
+        Lab5Box box1 = new Lab5Box(5, 3, 2);
         System.out.println("Volume of Box 1: " + box1.volume());
 
-        Box box2 = new Box(4, 2);
+        Lab5Box box2 = new Lab5Box(4, 2);
         System.out.println("Volume of Box 2: " + box2.volume());
 
-        Box box3 = new Box(3);
+        Lab5Box box3 = new Lab5Box(3);
         System.out.println("Volume of Box 3: " + box3.volume());
     }
 }
